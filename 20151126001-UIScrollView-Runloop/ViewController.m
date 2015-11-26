87ebe5924adf7b192b7013bebfd7fc6866656c0e
raineryue <jinlei_yue@sina.com>
2015-11-26 17:52:43 +0800

    UIScrollView练习＋运行循环的应用＝倒计时
    1.UIScrollView练习
    2.运行循环的应用

--- 20151126001-UIScrollView-Runloop/ViewController.m
+++ 20151126001-UIScrollView-Runloop/ViewController.m
@@ -10,13 +10,58 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UILabel *timeLabel;
+@property (strong, nonatomic) NSTimer *timer;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+/**
+ *  开始倒计时
+ */
+- (IBAction)playTimerClickAction {
+    // 创建一个默认运行循环状态的定时器
+//    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(startTimerAction:) userInfo:@"dajishi" repeats:YES];
+    
+    self.timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(startTimerAction:) userInfo:@"dajishi" repeats:YES];
+    
+    // 将定时器添加到NSRunLoopCommonModes类型的定时器中去，防止用户其它操作时，定时器不执行
+    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+}
+
+/**
+ * 暂停定时器
+ */
+- (IBAction)pauseClickAction{
+    [self.timer invalidate];
+}
+
+/**
+ *  停止定时器
+ */
+- (IBAction)stopClickAction {
+    [self pauseClickAction];
+    
+    self.timeLabel.text = @"10";
+}
+
+/**
+ * 开始倒计时
+ */
+- (void)startTimerAction:(NSTimer *)timer {
+    int timeCount = self.timeLabel.text.intValue;
+    
+    self.timeLabel.text = [NSString stringWithFormat:@"%d", --timeCount];
+    
+    if (timeCount == 0) {
+        [self pauseClickAction];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
